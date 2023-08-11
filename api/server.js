83dc3e844da01f9fd12e8a29fba54bcd04fd01ef
Dhiullian Santos <dhiullian.santos@pris.com.br>
2023-08-11 14:09:24 -0300

    changes db and route

--- api/server.js
+++ api/server.js
@@ -6,10 +6,17 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+
+server.get('/api/account/getDynamicMenu', (req, res) => {
+    const db = router.db;
+    const data = db.get('api.account.getDynamicMenu').value();
+    res.json(data);
+  });
+  
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
